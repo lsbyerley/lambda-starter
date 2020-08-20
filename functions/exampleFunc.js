52ee0c1643a70e7e69483ad2ee585c40a90d806e
Lucas <lsbyerley@gmail.com>
2020-08-20 11:07:28 -0400

    initial commit

---
+++ functions/exampleFunc.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+const { TEST_ENV_VAR } = process.env;
+
+const exampleFunc = async () => {
+
+  console.log('TEST ENV VAR: ', TEST_ENV_VAR);
+  const response = await fetch('https://api.ipify.org?format=json');
+  const json = await response.json();
+
+  return json;
+
+};
+
+module.exports = exampleFunc;
